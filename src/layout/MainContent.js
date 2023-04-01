6701e2fc69faa8ec12d44461558210e843236abe
Wojciech Wojnar <wojtek858@gmail.com>
2023-04-01 18:40:08 +0200

    updated layout

--- src/layout/MainContent.js
+++ src/layout/MainContent.js
@@ -1,48 +1,64 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getLastPosts, getTopPosts } from "../api/requests";
+import { getArticles } from "../api/requests";
 import Loader from "../components/Loader";
 import Panel from "../components/Panel";
 import Post from "../components/Article";
 import classes from "../styles/MainContent.module.css";
+import { addArticles } from "../store/newsReducer";
+import API from "../api/constants";
+import { useDispatch, useSelector } from "react-redux";
 
 const MainContent = () => {
-    let [articles, setArticles] = useState([]);
+    const { countryCode = "us" } = useParams();
+    const dispatch = useDispatch();
     let [loaderVisible, setLoaderVisible] = useState(true);
     let [errorMsg, setErrorMsg] = useState();
-    let [isList, setIsList] = useState(true);
-    const { countryCode } = useParams();
 
-    const changeView = () => {
-        setIsList(isList ? false : true)
-    }
+    const news = useSelector(state => state.news);
+    const viewType = useSelector(state => state.viewType);
+    let articles;
 
     useEffect(_ => {
-        getTopPosts(countryCode)
+
+        // check if store contains recent news for selected country
+        if (Object.hasOwn(news, countryCode)) {
+            let updatedAt = news[countryCode].updatedAt;
+
+            updatedAt = new Date(updatedAt).getTime();
+            const timeNow = new Date().getTime();
+
+            if (timeNow - updatedAt <= API.CACHE_LIFE) {
+                console.debug("Using cached articles");
+                setErrorMsg(null);
+                articles = news[countryCode].articles;
+                return;
+            };
+        }
+
+        getArticles(countryCode)
             .then(data => {
-                setErrorMsg();
+                setErrorMsg(null);
                 setLoaderVisible(false);
-                console.log(1, data);
+                if (!data) return;
                 if (Object.hasOwn(data, "error")) throw data;
-                setArticles(data.articles);
+                dispatch(addArticles({
+                    countryCode: countryCode,
+                    articles: [...data.articles]
+                }));
             })
             .catch(err => {
                 console.error(err);
-                if(Object.hasOwn(err, "error")) setErrorMsg(err.error);
-                else setErrorMsg("Unknown error, check console");
+                if (Object.hasOwn(err, "error")) setErrorMsg(err.error);
+                else setErrorMsg("ERROR", err);
             });
     }, [countryCode]);
 
     return (
-        <div className={isList ? classes.listView : classes.panelsView}>
-            <div onClick={changeView}>AAAAAAAAAAAA</div>
+        <div className={viewType === "list" ? classes.listView : classes.panelsView}>
             {loaderVisible && <Loader />}
             {errorMsg && <Panel title="Error">{errorMsg}</Panel>}
-            {articles && articles.map((article, index) => {
-                // const trimmedContent = post.content.substring(0, maxPostLength) + (post.content.length > 256 ? "..." : "")
-
-                return <Post key={index} article={article} />
-            })}
+            { (Object.hasOwn(news, countryCode) && news[countryCode].articles) && news[countryCode].articles.map((article, index) => <Post key={index} article={article} />)}
         </div>
     );
 }
