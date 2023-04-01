36c5be60096b061d1121e0f2d01aefa0adfaca3e
Wojciech Wojnar <wojtek858@gmail.com>
2023-04-01 18:49:02 +0200

    updated About page

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,9 @@ const About = ({ hidePopup }) => {
             <p>I created this project in 2 days after regular work, unfortunately late due to release of new product at work this week and lack of free time after work.
                 What I liked the most was creating React components, CSS design, writing JS functions and working with good API.</p>
 
-            <p>The part that I disliked the most was the fact, that I didn't know Redux Toolkit and I had very little time to learn it and Indian videos were very long.</p>
+            <p>The part that I disliked the most was the fact, that I didn't know Redux Toolkit and I had very little time to learn it and Indian videos were very long. It took me around 12 hours of programming, not counting hours spent on learning.</p>
+
+            <p>Website uses API caching and keeps articles for 30 minutes before refreshing, especially due to very high limitations of free news APIs, that limit requests to 100 per day.</p>
         </Popup>
     )
 }
